c1b6e97702c1a202fec57551141acf4e7b9fc26d
amer <amer@amer.com>
2023-12-30 13:51:36 +0300

    v0.2 changed the UI the currencyes are searchable now refered my accounts
    changed the name of the app
    added ico to it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("androidx.core:core-ktx:1.10.1")
@@ -61,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/basicconvertor/MainActivity.kt
+++ app/src/main/java/com/example/basicconvertor/MainActivity.kt
@@ -1,62 +1,123 @@
 package com.example.basicconvertor
 
 import DataCollector
+import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
+import android.widget.ImageView
+import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.basicconvertor.ui.theme.BasicConvertorTheme
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity(), AdapterView.OnItemSelectedListener{
     val convertor = DataCollector()
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         runBlocking {
             convertor.build()
         }
-        val keys : Array<String>  = convertor.rateCode?.keys?.map { it.toString() }?.toTypedArray()?: arrayOf()
+        val keys: Array<String> =convertor.rateCode?.keys?.map { it.toString() }?.toTypedArray() ?: arrayOf()
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,keys)
+        //this is the dialog for selecting currency
+        fun showMyDialog(text : TextView): View.OnClickListener { return View.OnClickListener {
+            val dialog = Dialog(this@MainActivity)
 
+            dialog.setContentView(R.layout.spinner_dialog)
+            dialog.window?.setLayout(800,1200)
+
+            dialog.show()
+            val list : ListView = dialog.findViewById(R.id.list)
+            val searchCur : EditText = dialog.findViewById(R.id.edit_text)
+
+            list.adapter = adapter
+
+            //search filterize method
+            searchCur.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    // Code to execute before the text changes
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    adapter.filter.filter(s)
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+
+                }
+            })
+
+            list.setOnItemClickListener(object : AdapterView.OnItemClickListener{
+                override fun onItemClick(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    text.setText(adapter.getItem(position))
+                    dialog.dismiss()
+
+                }
+            })
+        }
+        }
 
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.simplelayout)
-        val fromS : Spinner = findViewById(R.id.fromT)
-        val toS : Spinner = findViewById(R.id.toT)
 
-        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,keys)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
-        fromS.adapter = adapter
-        toS.adapter = adapter
-        fromS.onItemSelectedListener = this
-        toS.onItemSelectedListener = this
 
-        val amount : EditText = findViewById(R.id.editTextText)
-        val result : TextView = findViewById(R.id.textView3)
-        val conv : Button = findViewById(R.id.button)
-        conv.setOnClickListener(){
-            fromS.selectedItem
-            toS.selectedItem
+        val fromS: TextView = findViewById(R.id.curFrom)
+        val toS: TextView = findViewById(R.id.curTo)
+
+
+        val amount: EditText = findViewById(R.id.editTextText)
+        val result: TextView = findViewById(R.id.textView3)
+        val conv: Button = findViewById(R.id.button)
+        this.findViewById<ImageView>(R.id.x).setOnClickListener(){
+            openWebPage("https://x.com/73azn")
+        }
+        this.findViewById<ImageView>(R.id.insta).setOnClickListener(){
+            openWebPage("https://www.instagram.com/73azn/")
+        }
+        this.findViewById<ImageView>(R.id.github).setOnClickListener(){
+            openWebPage("https://github.com/73azn")
+        }
+        fromS.setOnClickListener(showMyDialog(fromS))
+        toS.setOnClickListener(showMyDialog(toS))
+        conv.setOnClickListener() {
+
             amount.text
-            result.setText(String.format("%.3f",convertor.convert(amount.text.toString().toDouble(),fromS.selectedItem.toString(),toS.selectedItem.toString())))
+            result.setText(
+                "${String.format("%.2f",convertor.convert(amount.text.toString().toDouble(),fromS.text.toString(),toS.text.toString()))} ${toS.text}")
+
+        //under here are the spinner dialog
+
+
+
+
 
         }
     }
+
+
+    private fun openWebPage(url: String) {
+        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(webIntent)
+    }
+
+
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
@@ -65,5 +126,10 @@ class MainActivity : ComponentActivity(), AdapterView.OnItemSelectedListener{
     override fun onNothingSelected(parent: AdapterView<*>?) {
 
     }
+
+}
+
+private fun ImageView.setOnClickListener() {
+
 }
 
