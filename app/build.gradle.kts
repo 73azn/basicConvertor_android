d7d4330734cd6df681a57bc648da6c82858312f2
AZIM <amerbinazim@gmail.com>
2024-01-25 21:09:37 +0300

    v1.8 updated dependencies Tested-by: amer <amer@amer.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,14 +11,14 @@ android {
         applicationId = "com.firstApp.basicconvertor"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6 "
+        versionCode = 9
+        versionName = "1.8 "
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
-        versionNameSuffix = "(closed testing)"
+        versionNameSuffix = "(open testing)"
     }
 
     buildTypes {
@@ -53,11 +53,11 @@ android {
 dependencies {
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
-    implementation("com.google.code.gson:gson:2.8.9")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
